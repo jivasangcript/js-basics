be2124fb445edd4aaa465b8f7354a544faf6d88c
Redonearth <wona23@gmail.com>
2022-11-15 20:17:28 +0900

    시간 표기 로직 수정

--- js/clock.js
+++ js/clock.js
@@ -1,14 +1,11 @@
-const clockContainer = document.querySelector('.js-clock'),
-  clockTitle = clockContainer.querySelector('span');
+const clockContainer = document.querySelector('.js-clock');
+const clockTitle = clockContainer.querySelector('span');
 
 function getTime() {
   const date = new Date();
-  const minutes = date.getMinutes();
-  const hours = date.getHours();
-  const seconds = date.getSeconds();
-  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
-    minutes < 10 ? `0${minutes}` : minutes
-  }:${seconds < 10 ? `0${seconds}` : seconds}`;
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  clockTitle.innerText = `${hours}:${minutes}`;
 }
 
 function init() {
