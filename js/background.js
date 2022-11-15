341d51f6b643fabe74b19c69d9cdffd4c45253bd
Redonearth <wona23@gmail.com>
2022-11-15 22:54:19 +0900

    마크업, 스타일링 수정

---
+++ js/background.js
@@ -0,0 +1,67 @@
+const UNSPLASH_API_KEY = '9PMZV9EvKnSVqCwZV0UG3XHKi0N27bJYydCePB1_y1Y';
+const UNSPLASH_URL = `https://api.unsplash.com/photos/random/?client_id=${UNSPLASH_API_KEY}&query=landscape&orientation=landscape`;
+
+const BG_IMAGE = 'bg-image';
+const savedImage = localStorage.getItem(BG_IMAGE);
+
+const body = document.querySelector('body');
+const locationContainer = document.querySelector(
+  '.js-location .location__text'
+);
+
+function getBackground() {
+  fetch(UNSPLASH_URL)
+    .then((response) => response.json())
+    .then((json) => {
+      const { urls, location, description } = json;
+
+      if (urls && location) {
+        const { full: fullUrl } = urls;
+        const { name } = location;
+        saveBackground(fullUrl, name, description);
+      } else {
+        getBackground();
+      }
+    });
+}
+
+function saveBackground(url, name, description) {
+  if (savedImage !== null) {
+    localStorage.removeItem(BG_IMAGE);
+  }
+
+  const expirationDate = new Date();
+  expirationDate.setDate(expirationDate.getHours() + 1);
+
+  const image = {
+    url,
+    name,
+    description,
+    expiresOn: expirationDate,
+  };
+
+  localStorage.setItem(BG_IMAGE, JSON.stringify(image));
+  loadBackground();
+}
+
+function loadBackground() {
+  if (savedImage === null) {
+    getBackground();
+  } else {
+    const { url, name, description, expiresOn } = JSON.parse(savedImage);
+    const today = new Date();
+
+    if (today.getHours() > expiresOn) {
+      getBackground();
+    } else {
+      body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(${url})`;
+      locationContainer.innerHTML = name ? name : description;
+    }
+  }
+}
+
+function init() {
+  loadBackground();
+}
+
+init();
---
+++ js/greetings.js
@@ -0,0 +1,53 @@
+const nameContainer = document.querySelector('.js-name');
+const USERNAME = 'username';
+
+function saveName(name) {
+  localStorage.setItem(USERNAME, name);
+}
+
+function renderName(name) {
+  nameContainer.innerHTML = '';
+  const nameText = document.createElement('span');
+  nameText.className = 'name__text';
+  nameText.innerHTML = `안녕하세요, ${name}!`;
+  nameContainer.appendChild(nameText);
+}
+
+function renderInput() {
+  const input = document.createElement('input');
+  const form = document.createElement('form');
+
+  input.type = 'text';
+  input.className = 'name__input';
+  input.placeholder = '당신의 이름은 뭔가요?';
+
+  form.addEventListener('submit', handleSubmit);
+  form.appendChild(input);
+  nameContainer.appendChild(form);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const { target } = event;
+  const input = target.querySelector('input');
+  const { value } = input;
+
+  saveName(value);
+  renderName(value);
+}
+
+function loadName() {
+  const username = localStorage.getItem(USERNAME);
+  if (username === null) {
+    renderInput();
+  } else {
+    renderName(username);
+  }
+}
+
+function init() {
+  loadName();
+}
+
+init();
